4e3024f56b8b67028b066aad59fe4909c58aa7fa
Andrii <andrii.dev13@gmail.com>
2025-09-20 22:57:15 +0200

    fix: add sanity to next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import {NextConfig} from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
  
 const nextConfig: NextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ["sanity", "next-sanity"],
+  },
 };
  
 const withNextIntl = createNextIntlPlugin();
