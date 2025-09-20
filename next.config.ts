b6ea08ff2117a277509eb04c712a60890545697d
Andrii <andrii.dev13@gmail.com>
2025-09-20 23:03:45 +0200

    fix sanity config

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,9 @@ import {NextConfig} from 'next';
 import createNextIntlPlugin from 'next-intl/plugin';
  
 const nextConfig: NextConfig = {
+  transpilePackages: ["sanity", "next-sanity"],
   experimental: {
-    transpilePackages: ["sanity", "next-sanity"],
+    serverComponentsExternalPackages: ["@sanity/client"],
   },
 };
  
