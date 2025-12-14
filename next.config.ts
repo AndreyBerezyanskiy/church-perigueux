c39475446af0d07786c079281b788ff91a52c931
Andrii <andrii.dev13@gmail.com>
2025-12-14 14:39:50 +0100

    fix config next

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,17 @@
-import {NextConfig} from 'next';
-import createNextIntlPlugin from 'next-intl/plugin';
- 
-const nextConfig: NextConfig = {
-  // У Next 15 це топ-левел, НЕ в experimental
-  transpilePackages: ["sanity", "next-sanity", "@sanity/ui", "@sanity/icons", "@sanity/color"],
+import type { NextConfig } from 'next'
+import createNextIntlPlugin from 'next-intl/plugin'
 
-  // Якщо імпортуєш @sanity/client на сервері — ок
-  experimental: {
-    serverComponentsExternalPackages: ["@sanity/client"],
-  },
-};
- 
-const withNextIntl = createNextIntlPlugin();
-export default withNextIntl(nextConfig);
+const nextConfig: NextConfig = {
+  transpilePackages: [
+    'sanity',
+    'next-sanity',
+    '@sanity/ui',
+    '@sanity/icons',
+    '@sanity/color',
+  ],
+
+  serverExternalPackages: ['@sanity/client'],
+}
+
+const withNextIntl = createNextIntlPlugin()
+export default withNextIntl(nextConfig)
